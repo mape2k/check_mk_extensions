6c39b05f6cd1076a842494a1a2ad2d06c493d577
Lars Falk-Petersen <dev@falkp.no>
2021-01-22 11:06:51 +0100

    issue211149 Prepare for prod deploy

--- exim_mailq/deploy.sh
+++ exim_mailq/deploy.sh
@@ -5,35 +5,28 @@
     exit 1;
 }
 
-agentname="agents/plugins/exim_mailq-test"
+agentnametest="agents/plugins/exim_mailq-test"
+agentname="agents/plugins/exim_mailq"
 checkname="checks/exim_mailq"
 watoplugin="wato/plugins/check_parameters_exim_mailq.py"
 checkman="checkman/exim_mailq"
 
 [[ "xdev" == "x$1" ]] && {
     # for testing and development. Not ment to be used by others
-
-    # local testing:
     scp $checkname test@omd.lxd:local/share/check_mk/checks/
-    scp $agentname web.lxd:/usr/lib/check_mk_agent/plugins/exim_mailq
+    scp $agentnametest web.lxd:/usr/lib/check_mk_agent/plugins/$agentname
     scp $watoplugin test@omd.lxd:local/share/check_mk/web/plugins/wato/
     scp $checkman test@omd.lxd:local/share/check_mk/checkman/
-
     exit 0;
 }
 
-# [[ "xprod" == "x$1" ]] && {
-#     # push to check_mk servers
-#     scp $checkname front@chk01.copyleft.no:local/share/check_mk/checks/
-#     #ssh front@chk01.copyleft.no $depsinstall
-
-#     # Checks from front should be synced here over time, but this makes it faster:
-#     scp $checkname cl@omdproxy01.copyleft.no:/omd/sites/cl/local/share/check_mk/checks/
-#     #ssh root@omdproxy01.copyleft.no $depsinstall
+[[ "xprod" == "x$1" ]] && {
+    # push to check_mk servers
+    scp $checkname front@chk01.copyleft.no:local/share/check_mk/checks/
+    # Checks from front should be synced here over time, but this makes it faster:
+    scp $checkname cl@omdproxy01.copyleft.no:/omd/sites/cl/local/share/check_mk/checks/
 
 #     scp $checkname root@icinga01.copyleft.no:/usr/share/check_mk/checks/
-#     #ssh root@icinga01.copyleft.no $depsinstall
-
 #     # push to check_mk proxies
 #     scp $checkname root@chkp01.telefactory:/usr/share/check_mk/checks/
 #     ssh root@chkp01.telefactory $depsinstall
@@ -52,8 +45,8 @@ checkman="checkman/exim_mailq"
 #     # Deploy agent to legacy copyleft puppet
 #     rsync -a $agentname larsfp@terminalserver1013.copyleft:git/puppet-checkmk/files/plugins/webchecks-agent.py && echo "puppet-checkmk need commit & push"
 
-#     exit 0;
-# }
+    exit 0;
+}
 
 echo "What did you do?"
 exit 1;
